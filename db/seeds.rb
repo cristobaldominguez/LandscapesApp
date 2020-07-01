e3f2118eec7ad51c69a8dfad86ae34ae5ea962c4
Cristobal Dominguez <cristobald@gmail.com>
2020-06-30 22:15:13 -0400

    Adds First User to Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create(email: "admin@example.com", name: 'Desafío', lastname: 'Latam', nickname: 'dl', password: 123456 )
