91ebb6c51595e5431a0b40b3c83dd9f69521a8b8
Cristobal Dominguez <cristobald@gmail.com>
2020-06-30 22:37:32 -0400

    Adds Faker and 10 New Users

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'jbuilder', '~> 2.5'
 # Adds Devise authentication
 gem 'devise'
 
+# Adds Faker
+gem 'faker', git: 'https://github.com/faker-ruby/faker.git', branch: 'master'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,5 +9,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+User.create(email: 'admin@example.com', name: 'Desafío', lastname: 'Latam', nickname: 'dl', password: 123_456)
 
-User.create(email: "admin@example.com", name: 'Desafío', lastname: 'Latam', nickname: 'dl', password: 123456 )
+10.times do
+  name = Faker::Name.first_name
+  lastname = Faker::Name.last_name
+  nickname = "#{name.downcase}_#{lastname.downcase}"
+
+  User.create(email: Faker::Internet.unique.email, name: name, lastname: lastname, nickname: nickname, password: 123_456)
+end
