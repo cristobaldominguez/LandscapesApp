ac63ae084e0f2fd4ff963f23c9ccb6cb03c4ce92
Matias Seguel <mseguelestay@gmail.com>
2020-07-02 22:06:03 -0400

    Add categories to seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :landscapes
+  has_many :landscapes, dependent: :destroy
   has_many :comments
   has_many :likes
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,26 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'Destroying records and data'
+Category.destroy_all
 User.destroy_all
-User.create(email: 'admin@example.com', name: 'Desafío', lastname: 'Latam', nickname: 'dl', password: 123456)
 
+puts 'Creating categories'
+categories = ["Naturaleza", "Ciudad", "Urbano", "Viajes", "Playas"]
+categories.each do |category|
+  Category.create!(name: category)
+end
+
+puts 'Creating Main User'
+User.create!(email: 'admin@example.com', name: 'Desafío', lastname: 'Latam', nickname: 'dl', password: 123456)
+
+puts 'Creating Faker Users'
 10.times do
   name = Faker::Name.first_name
   lastname = Faker::Name.last_name
   nickname = "#{name.downcase}_#{lastname.downcase}"
 
-  User.create(email: Faker::Internet.unique.email, name: name, lastname: lastname, nickname: nickname, password: 123_456)
+  User.create!(email: Faker::Internet.unique.email, name: name, lastname: lastname, nickname: nickname, password: 123_456)
 end
+
+puts 'DONE!'
