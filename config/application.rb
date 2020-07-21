f5280cbd215a0f1f81a2b199204abd3e3368b45f
Matias Seguel <mseguelestay@gmail.com>
2020-07-21 18:42:03 -0400

    Add cross origin security bypass

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module LandscapeApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
