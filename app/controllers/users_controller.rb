ffb76a1545262b87c329e2d0a1d42cb0b0dbd057
Cristobal Dominguez <cristobald@gmail.com>
2020-08-06 21:35:45 -0400

    WIP: Add Profile Edition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :lastname, :nickname, :email)
+      params.require(:user).permit(:name, :lastname, :nickname, :email, :description, :profile)
     end
 end
